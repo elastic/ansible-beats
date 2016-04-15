39d6d899f4ae90b4d923630c6387d5adc2d067e7
Dale McDiarmid <dalem@elastic.co>
2016-04-15 16:42:00 +0100

    more tests

--- test/integration/config/serverspec/default_spec.rb
+++ test/integration/config/serverspec/default_spec.rb
@@ -2,35 +2,27 @@ require 'spec_helper'
 
 describe 'Standard Tests' do
 
-  describe service('filebeat') do
+  describe service('packetbeat') do
     it { should be_running }
   end
 
-  describe package('filebeat') do
+  describe package('packetbeat') do
     it { should be_installed }
   end
 
-  describe file('/etc/filebeat/filebeat.yml') do
+  describe file('/etc/packetbeat/packetbeat.yml') do
     it { should be_file }
     it { should be_owned_by 'root' }
   end
 
-  describe file('/etc/filebeat/filebeat.yml') do
-    it { should contain 'filebeat:' }
+  describe file('/etc/packetbeat/packetbeat.yml') do
+    it { should contain 'packetbeat:' }
     it { should contain 'shipper:' }
     it { should contain 'logging:' }
     it { should contain 'output:' }
   end
 
-  describe file('/etc/init.d/filebeat') do
-    it { should exist }
-  end
-
-  describe file('/etc/default/filebeat') do
-    it { should exist }
-  end
-
-  describe file('/usr/lib/systemd/system/filebeat.service') do
+  describe file('/etc/init.d/packetbeat') do
     it { should exist }
   end
 
--- test/integration/multi/serverspec/default_spec.rb
+++ test/integration/multi/serverspec/default_spec.rb
@@ -26,13 +26,36 @@ describe 'Standard Tests' do
     it { should exist }
   end
 
-  describe file('/etc/default/filebeat') do
+  describe service('topbeat') do
+    it { should be_running }
+  end
+
+  describe package('topbeat') do
+    it { should be_installed }
+  end
+
+  describe file('/etc/topbeat/topbeat.yml') do
+    it { should be_file }
+    it { should be_owned_by 'root' }
+  end
+
+  describe file('/etc/topbeat/topbeat.yml') do
+    it { should contain 'input:' }
+    it { should contain 'period: 10' }
+    it { should contain 'stats:' }
+    it { should contain 'system: true' }
+    it { should contain 'process: true' }
+    it { should contain 'filesystem: true' }
+    it { should contain 'cpu_per_core: false' }
+    it { should contain 'shipper:' }
+    it { should contain 'logging:' }
+    it { should contain 'output:' }
+  end
+
+  describe file('/etc/init.d/topbeat') do
     it { should exist }
   end
 
-  describe file('/usr/lib/systemd/system/filebeat.service') do
-    it { should exist }
-  end
 
 end
 
